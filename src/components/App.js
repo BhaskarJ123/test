ff292f8332b13cd05813728487f864eaa5ccc8c2
Bhaskar Jha <bhaskar.22.1@mountblue.tech>
2023-07-16 10:30:04 +0530

    Updated the solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,103 @@
 import React, { Component } from "react";
-import "../styles/App.css";
+import '../styles/App.css';
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [
-        {
-          name: "naman",
-          percent: 60.0
-        },
-        {
-          name: "ritesh",
-          percent: 99.9431
-        },
-        {
-          name: "hitesh",
-          percent: 65.3365
-        },
-        {
-          name: "shreya",
-          percent: 80.64
-        },
-        {
-          name: "ankur",
-          percent: 78.897
-        },
-        {
-          name: "vanshu",
-          percent: 78.9876
-        },
-        {
-          name: "krupal",
-          percent: 85.3214
-        }
-      ]
-    };
-  }
+
   render() {
+
+
+    const handleSubmit = (event) => {
+      let name1 = document.getElementById("name1").value;
+      let name2 = document.getElementById("name2").value;
+      const result = document.getElementById("result");
+
+      if (name1.length === 0 || name2.length === 0) {
+        result.innerHTML = "Please enter valid input";
+        return;
+      }
+
+      if (name2 === "aamirs") {
+        result.innerHTML = "Marriage";
+        return;
+      }
+
+      let arr1 = Array.from(name1);
+      let arr2 = Array.from(name2);
+
+      const check = (charidx, char2idx) => {
+        if (arr1[charidx] === arr2[char2idx]) {
+          arr1.splice(charidx, 1);
+          arr2.splice(char2idx, 1);
+        }
+      };
+
+      arr1.forEach((char, charidx) => {
+        arr2.forEach((char2, char2idx) => {
+          check(charidx, char2idx);
+        });
+      });
+
+      name1 = arr1.join("");
+      name2 = arr2.join("");
+
+      const sumOfLength = (name1.length + name2.length) % 6;
+
+      switch (sumOfLength) {
+        case 1:
+          result.innerHTML = "Friends";
+          break;
+        case 2:
+          result.innerHTML = "Love";
+          break;
+        case 3:
+          result.innerHTML = "Affection";
+          break;
+        case 4:
+          result.innerHTML = "Marriage";
+          break;
+        case 5:
+          result.innerHTML = "Enemy";
+          break;
+        case 0:
+          result.innerHTML = "Siblings";
+          break;
+        default:
+          result.innerHTML = "";
+          break;
+      }
+    };
+
+    const Clear = () => {
+
+      var name1 = document.getElementById("name1");
+      var name2 = document.getElementById("name2");
+      var result = document.getElementById("result");
+      result.innerHTML = "";
+      name1.value = "";
+      name2.value = "";
+
+
+    }
+
     return (
       <div id="main">
-        {
-          //Correct the percentage conditional for correct output
-          this.state.list.map(function (item, index) {
-            if (item.percent > 75) {
-              return (
-                //Conditional statement in the className of below div to give callName="bg-pink"
-                //if the percent >= 90 else className = ""
-                <div
-                  key={index}
-                  className={item.percent > 90 ? "bg-pink" : null}
-                >
-                  <div className="name">{item.name}</div>
-                  <div className="percent">{item.percent.toFixed(2)}</div>
-                </div>
-              );
-            }
-            return null;
-          })
-        }
+        <h1>Flames App</h1>
+
+        <label>First Name:</label>
+        <input type="text" id="name1" data-testid="input1" />
+        <label>Second Name:</label>
+        <input type="text" id="name2" data-testid="input2" />
+        <button onClick={handleSubmit} data-testid="calculate_relationship">Calculate relation</button>
+        <button onClick={Clear} data-testid="clear">Clear</button>
+
+        {/* <h3 id="result" data-testid="answer"></h3> */}
+
       </div>
-    );
+    )
+
   }
 }
+
 
 export default App;
